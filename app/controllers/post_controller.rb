c5e3e42475db8d0b8d8fc4d6c32a9d329983f9be
i-cluster <jhlee9091@gmail.com>
2018-05-14 12:00:33 +0000

    article

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,2 +1,16 @@
 class PostController < ApplicationController
+    def index
+        @post = Post.all
+    end
+    
+    def new
+        post = Post.new
+        
+        post.title = params[:title]
+        post.text = params[:content]
+        
+        post.save
+        
+        redirect_to '/'
+    end
 end
--- db/migrate/20180514111306_create_posts.rb
+++ db/migrate/20180514111306_create_posts.rb
@@ -1,6 +1,8 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
+      t.string :title
+      t.string :text
 
       t.timestamps null: false
     end
